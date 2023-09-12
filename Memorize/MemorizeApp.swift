383c2015d2303eaeb439109d0b36f6cb3e98d1df
goptun <ramos.matheus@icloud.com>
2023-09-12 00:32:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matheus Ramos on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
